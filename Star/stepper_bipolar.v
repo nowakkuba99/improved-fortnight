1f4bd15518ada944c9a0f4e9029ce6160dc64071
Jakub Nowak <nowakkuba99@gmail.com>
2021-06-30 13:40:32 +0200

    Stepper Motor controler

--- Star/stepper_bipolar.v
+++ Star/stepper_bipolar.v
@@ -24,7 +24,95 @@ localparam S4 = 3'b100;
 //Aktualny Stan
 reg [3:0] r_State = S1;
 //Out
-reg [3:0]
+reg [3:0] r_out;
+//Kierunek
+reg r_dir;
+//Enable
+reg r_en;
+//Output assignment
+assign o_Big = r_out;
+
+//Main hardware loop
+always @(posedge i_clk) 
+begin
+    case (r_State)
+        S1: r_out <= 4'b1100;
+        S2: r_out <= 4'b0110;
+        S3: r_out <= 4'b0011;
+        S4: r_out <= 4'b1001;
+    endcase
+
+    //Określenie kierunku
+    if(i_control[0] == 1'b1 && i_control[1] == 1'b0)
+    begin
+        r_dir <=1'b1;         //Do przodu
+        r_en <= 1'b1;
+    end
+    else
+    if (i_control[0] == 1'b0 && i_control[1] == 1'b1) 
+    begin
+        r_dir <=1'b0;         //Do tyłu
+        r_en <= 1'b1;
+    end
+    else
+    begin
+        r_en <= 1'b0; 
+    end
+    case (r_State)
+        S1:
+        begin
+            if(r_dir == 1'b1 && r_en == 1'b1)
+            begin
+                r_State <=S2;
+            end
+            else
+            if(r_dir == 1'b0 && r_en == 1'b1)
+            begin
+                r_State <S4;
+            end
+        end
+
+        S2:
+        begin
+            if(r_dir == 1'b1 && r_en == 1'b1)
+            begin
+                r_State <=S3;
+            end
+            else
+            if(r_dir == 1'b0 && r_en == 1'b1)
+            begin
+                r_State <S1;
+            end
+        end
+
+        S3:
+        begin
+            if(r_dir == 1'b1 && r_en == 1'b1)
+            begin
+                r_State <=S4;
+            end
+            else
+            if(r_dir == 1'b0 && r_en == 1'b1)
+            begin
+                r_State <S2;
+            end
+        end
+
+        S4:
+        begin
+            if(r_dir == 1'b1 && r_en == 1'b1)
+            begin
+                r_State <=S1;
+            end
+            else
+            if(r_dir == 1'b0 && r_en == 1'b1)
+            begin
+                r_State <S3;
+            end
+        end    
+     
+    endcase
+end
 
 
     
