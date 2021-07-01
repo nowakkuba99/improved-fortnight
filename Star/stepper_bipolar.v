ec0ddca465c524d870102a982c0b963ca0824396
Jakub Nowak <nowakkuba99@gmail.com>
2021-07-01 13:12:02 +0200

    Development

--- Star/stepper_bipolar.v
+++ Star/stepper_bipolar.v
@@ -12,29 +12,57 @@ module stepper_driver (
     //output o_B,
     //output o_C,
     //output o_D,
-    output [3:0] o_Big
+    output [3:0] o_Big,
+    output o_pos
 );
 //Testowo okres pomiedzy krokami 1s - zegar 1Hz wchodzi
 localparam Ts = 1;
-//Aktualny Stan
+//Ilosc krokow - 200 kroków na obrót - 100 kroków = 180 stopni
+parameter  p_count_limit = 200;
+//Stany
 localparam S1 = 3'b001;
 localparam S2 = 3'b010;
 localparam S3 = 3'b011;
 localparam S4 = 3'b100;
+//Aktualny krok
+reg [7:0] r_counter =0;
 //Aktualny Stan
 reg [3:0] r_State = S1;
 //Out
 reg [3:0] r_out;
+reg r_pos;
 //Kierunek
 reg r_dir;
 //Enable
 reg r_en;
 //Output assignment
 assign o_Big = r_out;
-
+assign o_pos = r_pos;
 //Main hardware loop
 always @(posedge i_clk) 
 begin
+
+    //Określenie kierunku
+    if(i_control[0] == 1'b1 && i_control[1] == 1'b0 && r_counter != p_count_limit)
+    begin
+        r_dir <=1'b1;         //Do przodu
+        r_en <= 1'b1;
+    end
+    else
+    if (i_control[0] == 1'b0 && i_control[1] == 1'b1 && r_counter != 0) 
+    begin
+        r_dir <=1'b0;         //Do tyłu
+        r_en <= 1'b1;
+    end
+    else
+    begin
+        r_en <= 1'b0;         //Nieaktywny
+    end
+    //Sprawdzenie pozycji
+    if(r_counter == p_count_limit)
+    begin
+        r_pos <=2;
+    end
     case (r_State)
         S1: r_out <= 4'b1100;
         S2: r_out <= 4'b0110;
@@ -42,22 +70,7 @@ begin
         S4: r_out <= 4'b1001;
     endcase
 
-    //Określenie kierunku
-    if(i_control[0] == 1'b1 && i_control[1] == 1'b0)
-    begin
-        r_dir <=1'b1;         //Do przodu
-        r_en <= 1'b1;
-    end
-    else
-    if (i_control[0] == 1'b0 && i_control[1] == 1'b1) 
-    begin
-        r_dir <=1'b0;         //Do tyłu
-        r_en <= 1'b1;
-    end
-    else
-    begin
-        r_en <= 1'b0; 
-    end
+    
     case (r_State)
         S1:
         begin
@@ -68,7 +81,7 @@ begin
             else
             if(r_dir == 1'b0 && r_en == 1'b1)
             begin
-                r_State <S4;
+                r_State <=S4;
             end
         end
 
@@ -81,7 +94,7 @@ begin
             else
             if(r_dir == 1'b0 && r_en == 1'b1)
             begin
-                r_State <S1;
+                r_State <=S1;
             end
         end
 
@@ -94,7 +107,7 @@ begin
             else
             if(r_dir == 1'b0 && r_en == 1'b1)
             begin
-                r_State <S2;
+                r_State <=S2;
             end
         end
 
@@ -107,7 +120,7 @@ begin
             else
             if(r_dir == 1'b0 && r_en == 1'b1)
             begin
-                r_State <S3;
+                r_State =S3;
             end
         end    
      
