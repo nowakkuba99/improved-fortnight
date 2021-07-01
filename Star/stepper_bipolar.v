17b32b0957ffc331fc9743c9f32be1b2eb496a0d
Jakub Nowak <nowakkuba99@gmail.com>
2021-07-01 15:20:17 +0200

    After debugging

--- Star/stepper_bipolar.v
+++ Star/stepper_bipolar.v
@@ -13,12 +13,14 @@ module stepper_driver (
     //output o_C,
     //output o_D,
     output [3:0] o_Motor,
-    output [1:0] o_pos
+    output [1:0] o_pos,
+	//output [7:0] o_debug,
+	//output o_debug_en
 );
 //Testowo okres pomiedzy krokami 1s - zegar 1Hz wchodzi
 localparam Ts = 1;
 //Ilosc krokow - 200 kroków na obrót - 100 kroków = 180 stopni
-parameter  p_count_limit = 200;
+parameter  p_count_limit = 100;
 //Stany
 localparam S1 = 3'b001;
 localparam S2 = 3'b010;
@@ -38,27 +40,13 @@ reg r_en;
 //Output assignment
 assign o_Motor = r_out;
 assign o_pos = r_pos;
+//assign o_debug = r_counter;
+//assign o_debug_en = r_en;
 //Main hardware loop
 always @(posedge i_clk) 
 begin
 
-    //Określenie kierunku
-    if(i_control[0] == 1'b1 && i_control[1] == 1'b0 && r_counter != p_count_limit)
-    begin
-        r_dir <=1'b1;         //Do przodu
-        r_en <= 1'b1;
-    end
-    else
-    if (i_control[0] == 1'b0 && i_control[1] == 1'b1 && r_counter != 0) 
-    begin
-        r_dir <=1'b0;         //Do tyłu
-        r_en <= 1'b1;
-    end
-    else
-    begin
-        r_en <= 1'b0;         //Nieaktywny
-    end
-    //Sprawdzenie pozycji
+ //Sprawdzenie pozycji
     if(r_counter == 0)
     begin
         r_pos <= 2'b00;
@@ -72,6 +60,39 @@ begin
     begin
         r_pos <= 2'b10;
     end
+	 
+	 
+    //Określenie kierunku
+    if(i_control[2] == 1'b1 && i_control[3] == 1'b0 )
+    begin
+        r_dir <=1'b1;         //Do przodu
+		if(r_counter < p_count_limit-1)
+		begin
+        r_en <= 1'b1;
+		end
+		else
+		begin
+		r_en <= 1'b0;
+		end
+    end
+    else
+    if (i_control[2] == 1'b0 && i_control[3] == 1'b1) 
+    begin
+        r_dir <=1'b0;         //Do tyłu
+        if(r_counter > 1)
+		begin
+        r_en <= 1'b1;
+		end
+		else
+		begin
+		r_en <= 1'b0;
+		end
+    end
+	else
+	begin
+	r_en <=0;
+	end
+   
     //Ustawienie wyjść
     case (r_State)
         S1: r_out <= 4'b1100;
